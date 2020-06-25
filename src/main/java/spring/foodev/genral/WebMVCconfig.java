938d4c1776ab302f6cd49984d2d203d1745d5149
dogpaw <foodev@naver.com>
2020-06-25 21:31:13 +0900

    썸머노트 이미지 콜백 처리 추가

---
+++ src/main/java/spring/foodev/genral/WebMVCconfig.java
@@ -0,0 +1,20 @@
+package spring.foodev.genral;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/*
+@Configuration
+public class WebMVCconfig implements WebMvcConfigurer{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+
+		registry.addResourceHandler("/summernoteImage/**").addResourceLocations("file:///D:/summernote_image/");
+	}
+	
+	
+	
+}
+*/
--- src/main/java/spring/foodev/view/HomeController.java
+++ src/main/java/spring/foodev/view/HomeController.java
@@ -1,6 +1,9 @@
 package spring.foodev.view;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -8,11 +11,24 @@ import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
+import javax.annotation.Resource;
 import javax.inject.Inject;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.ibatis.javassist.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,6 +37,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.ServletConfigAware;
 import org.springframework.web.multipart.MultipartFile;
 
 
@@ -37,8 +54,10 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-
+	@Autowired
+	ServletContext sc;
 	
+
 	@Inject
 	private BoardDAO dao;
 	/**
@@ -89,26 +108,55 @@ public class HomeController {
 		return "hi";
 	}
 	
-	
 	@RequestMapping(value = "/board/uploadSummernoteImageFile", method = RequestMethod.POST)
-	public @ResponseBody HashMap<String, String> createImgUrl(@RequestParam("file") MultipartFile multipartFile ) {
+	public @ResponseBody HashMap<String, String> createImgUrl(@RequestParam("file") MultipartFile multipartFile ,HttpServletRequest req) {
 		
+		
+//		String saveFileRoot=req.getServletContext().getRealPath("resources/image/");
+		//System.out.println(req.getSession().getServletContext().getRealPath(""));
 		String saveFileRoot ="D:\\summernote_image\\";
-		String loadOriginalFileName = multipartFile.getOriginalFilename();
-		String extension = loadOriginalFileName.substring(loadOriginalFileName.lastIndexOf("."));
-		HashMap<String, String> map = new HashMap<String, String>();
-		String saveFileName = UUID.randomUUID() + extension;
+	
+
+
 		
+		String loadOriginalFileName = multipartFile.getOriginalFilename();
+		
+		String extension = loadOriginalFileName.substring(loadOriginalFileName.lastIndexOf("."));
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		String saveFileName = UUID.randomUUID() + extension;
+	
 		File targetFile  = new File(saveFileRoot+saveFileName);
+		
+		
+		System.out.println(targetFile);
+		
+		
+		try {
+ 			InputStream fileStream = multipartFile.getInputStream();
+		    FileUtils.copyInputStreamToFile(fileStream, targetFile);
+			map.put("url", "http://127.0.0.1:8080/board/summernote/"+saveFileName);
+		
+			
+			
+			
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	
+		
+		
 		System.out.println(multipartFile);
 		System.out.println(loadOriginalFileName);
-		map.put("url",saveFileName);
+	//	map.put("url",saveFileName);
 		
 		return map;
 	}
 	
 
-	@RequestMapping(value = "/test2", method = RequestMethod.GET)
+	@RequestMapping(value = "/board/3", method = RequestMethod.GET)
 	public @ResponseBody HashMap<String, Object> bo2ardListReturn(String sz) {
 	//	logger.info("게시판 전달");
 		
@@ -116,17 +164,17 @@ public class HomeController {
 		
 
 		
-	//	BoardVO a= new BoardVO();
+		BoardVO a= new BoardVO();
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		/*a.setDivision("시청하기");
+		a.setDivision("시청하기");
 		a.setPost_id(1);
 		a.setTitle("제목이다 쉭쉭버거");
 		a.setWriter("test");
-		a.setWrite_date("2020-06-09");
-		a.setUp_count(1);
-		a.setLookup_count(1);*/
 		
-		map.put("boardList",sz);
+		a.setUp_count(1);
+		a.setLookup_count(1);
+		
+		map.put("boardList",a);
 		
 		return map;
 	}
@@ -153,5 +201,23 @@ public class HomeController {
 		
 		return map;
 	}
+	
+	
+	@RequestMapping(value = "/enen", method = RequestMethod.GET)
+	public ResponseEntity<byte[]> testestet(String vo) throws IOException {
+	     HttpHeaders header = new HttpHeaders();
+	        header.setContentType(MediaType.IMAGE_PNG);
+	        
+	        
+	        ResponseEntity<byte[]> tesm= new ResponseEntity<byte[]>(IOUtils.toByteArray(new FileInputStream(new File("D:\\summernote_image\\90335f4d-b2d8-477c-b33b-c6b29df9bd18.png"))), header, HttpStatus.CREATED);
+	  
+	       
+	       System.out.println(tesm.getHeaders()); 
+	        return tesm;
+	        
+	        
+	}
+
+
 	
 }
